66c8c69674decde1dbef551de5b9b6ceff479d1d
Ali-akbark <115396873+Ali-akbark@users.noreply.github.com>
2025-04-09 11:04:07 +0530

    🔥 Full project upload

---
+++ test-env.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "C:\\Users\\Ali Akbar\\Downloads\\EcoBristleBrush (1)\\EcoBristleBrush\\.env"
+});
+
+console.log("✅ DATABASE_URL =", process.env.DATABASE_URL);
